31ada6458429e4ff936f35baa3087b2dc92b547e
Daniel Pfeifer <daniel@pfeifer-mail.de>
2015-12-02 00:16:25 +0100

    add QObject

---
+++ include/byom/ext/qobject.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2015, Daniel Pfeifer <daniel@pfeifer-mail.de>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef BYOM_EXT_QOBJECT_HPP
+#define BYOM_EXT_QOBJECT_HPP
+
+#include <byom/dynamic_view.hpp>
+#include <QObject>
+
+namespace byom {
+
+template <>
+struct ext<QObject const*> : detail::fallback
+{
+  static bool empty_impl(QObject const* model);
+
+  static dynamic_view at_impl(QObject const* model, std::string const& name);
+
+  static void for_each_impl(QObject const* model, visit_function const& visit);
+};
+
+} // namespace byom
+
+#endif /* BYOM_EXT_QOBJECT_HPP */
---
+++ test/qobject.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2015, Daniel Pfeifer <daniel@pfeifer-mail.de>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <byom/dynamic_view.hpp>
+#include <byom/ext/qobject.hpp>
+
+#include <boost/core/lightweight_test.hpp>
+#include <boost/lexical_cast.hpp>
+
+class QPerson : public QObject
+{
+  Q_OBJECT
+  Q_PROPERTY(QString name READ name WRITE setName)
+  Q_PROPERTY(int age READ age WRITE setAge)
+
+public:
+  QPerson(QObject* parent = nullptr)
+    : QObject(parent)
+  {
+  }
+
+  QString name() const
+  {
+    return m_name;
+  }
+
+  int age() const
+  {
+    return m_age;
+  }
+
+  void setName(QString name)
+  {
+    m_name = name;
+  }
+
+  void setAge(int age)
+  {
+    m_age = age;
+  }
+
+private:
+  QString m_name;
+  int m_age;
+};
+
+#include "qobject.moc"
+
+namespace byom {
+template <>
+struct ext<QPerson*> : ext<QObject const*>
+{
+};
+}
+
+int qobject(int argc, char* argv[])
+{
+  auto* model = new QPerson;
+  model->setName("John Doe");
+  model->setAge(42);
+
+  auto const view = byom::dynamic_view{ model };
+
+  BOOST_TEST(!view.empty());
+
+  BOOST_TEST_THROWS(boost::lexical_cast<std::string>(view),
+                    std::invalid_argument);
+
+  auto const name = view.at("name");
+  BOOST_TEST_EQ(boost::lexical_cast<std::string>(name), "John Doe");
+
+  auto const age = view.at("age");
+  BOOST_TEST_EQ(boost::lexical_cast<std::string>(age), "42");
+
+  BOOST_TEST_THROWS(view.at("bar"), std::out_of_range);
+
+  int counter = 0;
+  view.for_each([&](auto const& key, auto const& val) {
+    switch (++counter) {
+      case 1:
+        BOOST_TEST_EQ(boost::lexical_cast<std::string>(key), "name");
+        BOOST_TEST_EQ(boost::lexical_cast<std::string>(val), "John Doe");
+        break;
+      case 2:
+        BOOST_TEST_EQ(boost::lexical_cast<std::string>(key), "age");
+        BOOST_TEST_EQ(boost::lexical_cast<std::string>(val), "42");
+        break;
+      default:
+        BOOST_ERROR("Should not be reached.");
+        break;
+    }
+  });
+  BOOST_TEST_EQ(counter, 2);
+
+  delete model;
+
+  return boost::report_errors();
+}
